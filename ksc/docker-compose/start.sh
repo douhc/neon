b6a671859c6275e4023e974185cfc7300c08a675
douhongchen <douhongchen@kingsoft.com>
2024-08-02 11:01:46 +0800

    update compute spec

--- ksc/docker-compose/start.sh
+++ ksc/docker-compose/start.sh
@@ -2,5 +2,5 @@
 
 PG_VERSION=16  docker-compose --profile compute -f docker-compose.yml up --build -d
 PG_VERSION=16  docker-compose --profile compute -f docker-compose.yml logs
-
+# PG_VERSION=16  docker-compose scale compute=0
 # psql -h localhost -U cloud_admin -p 55433 -d postgres
